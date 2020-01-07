bd4edee2f3d934001bcf97430524386bb161dfea
Mars <houjianchao1204@gmail.com>
2020-01-07 14:25:53 +0800

    project init

---
+++ app/src/main/java/com/mars/multilanguage/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.mars.multilanguage.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val usernameError: Int? = null,
+                      val passwordError: Int? = null,
+                      val isDataValid: Boolean = false)
